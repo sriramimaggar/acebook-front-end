14ea935add8a6c6ef7cc0fa56e4209365e62e6e5
Captain-Raptor <leojones@imaggar.in>
2024-09-14 22:15:47 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Login from "./pages/login";
-import Register from "./pages/Register/Index";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from './pages/Dashboard';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* <Login /> */}
+      <Login />
       <Register />
+      <Dashboard />
     </>
   );
 }
